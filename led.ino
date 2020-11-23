29be15f406f873fbeb549560fdc5f10f6c6aacaf
DiZurix <dizurix@outlook.fr>
2020-11-23 16:09:15 +0100

    Add files via upload

--- led.ino
+++ led.ino
@@ -1,7 +1,7 @@
 const int RED_LED = 11;
 const int GREEN_LED = 10;
 
-int SWITCH_LED (const int LED){
+void SWITCH_LED (const int LED){
     digitalWrite(LED, HIGH);
     delay(1500);
     digitalWrite(LED, LOW);
