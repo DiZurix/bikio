983e9d489e0e7162cc3408306d6aad413b8ee5c4
malo marin <38227021+malo7414@users.noreply.github.com>
2020-11-27 10:26:19 +0100

    reajustement de la page d'acceuil + debut creation popup buttoninscription2

--- javascript/Formulaire.js
+++ javascript/Formulaire.js
@@ -4,7 +4,7 @@
   calculateHeight = function() {
     var $content, contentHeight, finalHeight, windowHeight;
     $content = $('#overlay-content');
-    contentHeight = parseInt($content.height()) + parseInt($content.css('margin-top')) + parseInt($content.css('margin-bottom'));
+    contentHeight = parseInt($content.height()) ;
     windowHeight = $(window).height();
     finalHeight = windowHeight > contentHeight ? windowHeight : contentHeight;
     return finalHeight;
@@ -12,13 +12,14 @@
 
   $(document).ready(function() {
     $(window).resize(function() {
-      if ($(window).height() < 560 && $(window).width() > 600) {
+      if ($(window).height() < 500 && $(window).width() > 500) {
         $('#overlay').addClass('short');
       } else {
         $('#overlay').removeClass('short');
       }
       return $('#overlay-background').height(calculateHeight());
     });
+    $(window).trigger('resize');
     $('#popup-trigger').click(function() {
       return $('#overlay').addClass('open').find('.signup-form input:first').select();
     });
