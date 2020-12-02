41d7d6955c8ddc318c48d5fb36314a2f8dab50fc
swagdrien <71891374+swagdrien@users.noreply.github.com>
2020-12-02 21:58:52 +0100

    Update rfid.ino
    avancement des traductions

--- rfid.ino
+++ rfid.ino
@@ -7,30 +7,27 @@
     This example will wait for any ISO14443A card or tag, and
     depending on the size of the UID will attempt to read from it.
    
-    If the card has a 4-byte UID it is probably a Mifare
-    Classic card, and the following steps are taken:
+    Si la carte a un UID sur 4 octets, c'est proçbablement une carte Mifare
+    Classic. Le cas échéant, suivre ces étapes:
    
-    - Authenticate block 4 (the first block of Sector 1) using
-      the default KEYA of 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
-    - If authentication succeeds, we can then read any of the
-      4 blocks in that sector (though only block 4 is read here)
+    - Authentifier le block 4 (pemier du secteur 1) avec
+      la clé par défaut : KEYA 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
+    - Si l'authentification est réussie, on peut maintenant lire les
+      4 blocks dans ce secteur (bien que seul le block 4 soit lu ici)
 	 
-    If the card has a 7-byte UID it is probably a Mifare
-    Ultralight card, and the 4 byte pages can be read directly.
+    Si la carte a un UID sur 7 octets, c'est probablement une carte Mifare
+    Ultralight , et les pages de 4 octets peuvent être lues directement.
     Page 4 is read by default since this is the first 'general-
     purpose' page on the tags.
 
 
-This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
-This library works with the Adafruit NFC breakout 
+Exemple de schéma pour l'Adafruit PN532 NFC/RFID breakout boards
+Cette library marche avec le breakout Adafruit NFC 
   ----> https://www.adafruit.com/products/364
  
 Check out the links above for our tutorials and wiring diagrams 
 These chips use SPI or I2C to communicate.
 
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
 
 */
 /**************************************************************************/
@@ -113,13 +110,13 @@ void loop(void) {
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
     
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  // Attente d'une carte type ISO14443A (Mifare, etc.).  Quand une carte est trouvée,
+  // 'uid' sera rempli avec le UID, et uidLength indiquera
+  // si le IUD est en 4o (Mifare Classic) ou 7o (Mifare Ultralight)
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
   
   if (success) {
-    // Display some basic information about the card
+    // Affiche les infos de base de la carte
     Serial.println("Found an ISO14443A card");
     Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
     Serial.print("  UID Value: ");
@@ -128,17 +125,17 @@ void loop(void) {
     
     if (uidLength == 4)
     {
-      // We probably have a Mifare Classic card ... 
+      // On a surement une carte "Mifare Classic"...
       Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
 	  
-      // Now we need to try to authenticate it for read/write access
-      // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
+      // On doit maintenant l'authentifier pour accéder aux lecture/écriture
+      // Essaie avec la clé par défaut d'usine KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
       Serial.println("Trying to authenticate block 4 with default KEYA value");
       uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
 	  
-	  // Start with block 4 (the first block of sector 1) since sector 0
-	  // contains the manufacturer data and it's probably better just
-	  // to leave it alone unless you know what you're doing
+	  // Essaie avec le block 4 (premier block du secteur 1) depuis secteur 0
+	  // contient la data constructeur, mieux vaut ne pas y toucher
+	  // à moins que tu saches ce que tu fais.
       success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
 	  
       if (success)
@@ -148,18 +145,18 @@ void loop(void) {
         int uid_verifnfc[5] = {'b', 'i', 'k', 'i', 'o'};
         int good_uid = 0;
 		
-        // If you want to write something to block 4 to test with, uncomment
-		// the following line and this text should be read back in a minute
+        // Pour écrire dans le block 4, tester avec :
+		// (ce qui suit devrait être lu en réponse)
               //ECRITURE RFID
         //memcpy(data, (const uint8_t[]){ 'b', 'i', 'k', 'i', 'o', '.', 'u', 's', 'e', 'r', '.', 'a', 'l', 'a', 'i', 'n' }, sizeof data);
         //success = nfc.mifareclassic_WriteDataBlock (4, data);
 
-        // Try to read the contents of block 4
+        // Essaie de lire le contenu du block 4
         success = nfc.mifareclassic_ReadDataBlock(4, data);
 		
         if (success)
         {
-          // Data seems to have been read ... spit it out
+          // Des données semblent avoir été lues... voyons voir
           Serial.println("Reading Block 4:");
           nfc.PrintHexChar(data, 16);
 
@@ -176,43 +173,43 @@ void loop(void) {
             SWITCH_LED(RED_LED);
           }
 
-          // Wait a bit before reading the card again
+          // Attendre un peu avant de relire la carte
           delay(1000);
         }
         else
         {
-          Serial.println("Ooops ... unable to read the requested block.  Try another key?");
+          Serial.println("Ooops ... impossible de lire le block demandé.  Essaie une autre clé ?");
           SWITCH_LED(RED_LED);
         }
       }
       else
       {
-        Serial.println("Ooops ... authentication failed: Try another key?");
+        Serial.println("Ooops ... échec de l'authetification. Essayer une autre clé ?");
         SWITCH_LED(RED_LED);
       }
     }
     
     if (uidLength == 7)
     {
-      // We probably have a Mifare Ultralight card ...
-      Serial.println("Seems to be a Mifare Ultralight tag (7 byte UID)");
+      // On a surement une carte Mifare Ultralight...
+      Serial.println("On dirait un tag Mifare Ultralight (7 byte UID)");
 	  
-      // Try to read the first general-purpose user page (#4)
-      Serial.println("Reading page 4");
+      // Essai de lecture de la page "à tout faire" utilisateur (#4)
+      Serial.println("lecture de la page 4");
       uint8_t data[32];
       success = nfc.mifareultralight_ReadPage (4, data);
       if (success)
       {
-        // Data seems to have been read ... spit it out
+        // Des données semblent avoir été lues... voyons voir
         nfc.PrintHexChar(data, 4);
         Serial.println("");
 		
-        // Wait a bit before reading the card again
+        // Attendre un peu avant de relire la carte
         delay(1000);
       }
       else
       {
-        Serial.println("Ooops ... unable to read the requested page!?");
+        Serial.println("Ooops ... impossible de lire la page demandée !?");
       }
     }
   }
